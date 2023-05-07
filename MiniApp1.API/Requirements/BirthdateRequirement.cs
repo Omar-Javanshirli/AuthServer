5efe3be34bac29d7f894ad2136e487c21fe49ad6
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-05-07 03:12:43 -0700

    new update

--- MiniApp1.API/Requirements/BirthdateRequirement.cs
+++ MiniApp1.API/Requirements/BirthdateRequirement.cs
@@ -1,6 +1,41 @@
-﻿namespace MiniApp1.API.Requirements
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace MiniApp1.API.Requirements
 {
-    public class BirthdateRequirement
+    public class BirthdateRequirement : IAuthorizationRequirement
     {
+        public int Age { get; set; }
+
+        public BirthdateRequirement(int age)
+        {
+            Age = age;
+        }
+
+        public class BirthdateRequirementHandler : AuthorizationHandler<BirthdateRequirement>
+        {
+            protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, BirthdateRequirement requirement)
+            {
+                var birthdate = context.User.FindFirst("birth-date");
+                if (birthdate == null)
+                {
+                    //endpointe istek gondere bilmirsen
+                    context.Fail();
+                    return Task.CompletedTask;
+                }
+
+                var toDay = DateTime.Now;
+                var age = toDay.Year - Convert.ToDateTime(birthdate.Value).Year;
+
+                if (age >= requirement.Age)
+                    context.Succeed(requirement);
+                else
+                    context.Fail();
+
+                //bos task classi qaytarir.
+                return Task.CompletedTask;
+            }
+        }
     }
 }
